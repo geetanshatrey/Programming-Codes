362226703644c412d2dbc841fe99f480f06f0a7b
Geetansh Atrey <atrey.geetansh@gmail.com>
2019-10-06 23:36:21 +0530

    Maps
    Simple program to demonstrate Maps in Java.

---
+++ MapEx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class MapEx
+{
+		public static void main(String x[])
+		{
+				Map map=new HashMap();
+				map.put("Father","PKA");
+				System.out.println(map.get("Father"));
+
+		}
+
+}
