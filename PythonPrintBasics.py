4c9001c2fea656c5899abbc3f596f054709b1570
Geetansh Atrey <atrey.geetansh@gmail.com>
2019-10-16 00:56:33 +0530

    Python Print Basics
    Print function in Python and the ways to use it.

---
+++ PythonPrintBasics.py
@@ -0,0 +1,15 @@
+_author_ = 'Geetansh'
+
+print('Hello World')
+print(1+2)
+print(40-10)
+print(12*5)
+print(45/9)
+print("We can also use double quotes to print a string.")
+print('We can even include "Quotes " between Quotes.')
+print("Hello"+"World!") 
+#Comments can be used using a hashtag(#)
+print("Hello"+" "+"World!")
+greeting="Hello"
+name="Geetansh!"
+print(greeting + " " + name)
