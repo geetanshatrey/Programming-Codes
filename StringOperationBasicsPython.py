86b8edbf3564726fae1d927a9511be79a7d490b1
Geetansh Atrey <atrey.geetansh@gmail.com>
2019-10-17 23:15:44 +0530

    Python Print Split and Tabbed
    Program to show the \n and \t.

---
+++ StringOperationBasicsPython.py
@@ -0,0 +1,9 @@
+_author_='Geetansh'
+
+splitTheString="This string will split now\nas I did something\nOh again!"
+tabTheString="This string will be tabbed\tnow. It did \t Again!"
+print(splitTheString)
+print(tabTheString)
+print("This string is used to show how to use \"Double Quotes within Double Quotes\"")
+
+# \"is used to use quotes inside quotes.
