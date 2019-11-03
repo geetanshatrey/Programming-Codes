309e57b7f33cd436b9301ed770b459d26cf26e2e
Geetansh Atrey <atrey.geetansh@gmail.com>
2019-11-04 02:16:10 +0530

    Dynamic Typing
    Program to show dynamic typing in Python.

---
+++ PythonDynamicTyping.py
@@ -0,0 +1,7 @@
+ch=10000;
+print("%d" %ch)
+#Python uses Dynamic Typing. Thus we can reassign variables to different data types.
+ch="Ten thousand"
+print(ch)
+#Python variable names cannot start with a number.
+#Cannot Use symbols such as :!@#%^&*()<>"";?
