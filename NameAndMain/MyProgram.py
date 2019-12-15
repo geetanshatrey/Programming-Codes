6ed28b42ffd5234188e2a29ee1a1a14a99628465
Geetansh Atrey <atrey.geetansh@gmail.com>
2019-12-15 11:03:06 +0530

    __name__ and "__main__"
    Python Programs to show the use of __name__ variable.

---
+++ NameAndMain/MyProgram.py
@@ -0,0 +1,3 @@
+from One import func
+
+func()
---
+++ NameAndMain/One.py
@@ -0,0 +1,10 @@
+#Python One.py 
+
+def func():
+    print("FUNC IN ONE.PY")
+
+print("Top level in One.py")
+if __name__=="__main__":
+    print("One.py run directly !!!")
+else:
+    print("One.py has been imported.")
