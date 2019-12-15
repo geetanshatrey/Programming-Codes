81336b5eadae7fab63738b181e80d20064b69408
Geetansh Atrey <atrey.geetansh@gmail.com>
2019-12-15 08:38:52 +0530

    Colorama Package
    Program to show the colorama package used to print the text in different colors in Python.

---
+++ ColoramaPackage.py
@@ -0,0 +1,8 @@
+from colorama import init
+init()
+from colorama import Fore
+print(Fore.RED + "Some Red Text")
+print(Fore.GREEN +"Switch to Green")
+print("Hello World")
+print(Fore.YELLOW +"Switch to Yellow")
+quit()
