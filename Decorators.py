3df53330c14e0850a587b50051c3b78ab6b2a55c
Geetansh Atrey <atrey.geetansh@gmail.com>
2019-12-20 09:06:40 +0530

    Decorators in Python
    Program to show decorators in Python.

---
+++ Decorators.py
@@ -0,0 +1,16 @@
+def new_decorator(original_func):
+
+    def wrap_func():
+        print('Some extra code, before the original')
+        original_func()
+        print('Some extra code, after the original')
+    
+    return wrap_func
+
+@new_decorator #Keyword.
+def func_needs_decorator():
+    print("I need decorator!")
+
+#decorated_func=new_decorator(func_needs_decorator) --> Another way of doing this.
+#decorated_func()
+func_needs_decorator()
