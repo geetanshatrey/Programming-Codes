c024cf836d31e39de69a6fcaf8210611ccd6fc9f
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-06-16 00:39:33 +0530

    Create PrintDetails.java

---
+++ PrintDetails.java
@@ -0,0 +1,8 @@
+class PrintDetails {
+    public static void main(String args[]) {
+        System.out.println("Geetansh Atrey");
+        System.out.println("001");
+        System.out.println("CSE-11");
+        System.out.println("Computer Science");
+    }
+}
