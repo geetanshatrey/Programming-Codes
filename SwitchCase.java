48555a24b7800a1c11d33ec04a2be0de6258ef54
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-06-16 00:39:42 +0530

    Create SwitchCase.java

---
+++ SwitchCase.java
@@ -0,0 +1,19 @@
+class SwitchCase {
+    public static void main(String args[]) {
+        int x = 3;
+        switch(x) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("bigger than 3");
+                break;
+        } 
+    }
+}
