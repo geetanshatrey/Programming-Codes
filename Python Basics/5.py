e2c8897b087c5479d4de92c0894bce768efecb93
Geetansh Atrey <atrey.geetansh@gmail.com>
2021-01-06 10:44:17 +0530

    Create 5.py

---
+++ Python Basics/5.py
@@ -0,0 +1,8 @@
+x = input("\nEnter the number 1 : ")
+a = int(x)
+x = input("\nEnter the number 2 : ")
+b = int(x)
+a = a + b
+b = a - b
+a = a - b
+print("\nSwapped Numbers : " + str(a) + " and " + str(b))
